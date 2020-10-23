9df1b2a9bcbdf9f71ec9c84eec106318eeff1c22
compy <blakemyates@hotmail.com>
2020-10-23 14:14:05 -0500

    reorganized files

---
+++ Assets/Scripts/BattleActorRelated/MaintainWorldRotation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaintainWorldRotation : MonoBehaviour
+{
+    void Update()
+    {
+        this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, this.transform.parent.rotation.z * -1.0f); //TODO: make this an event to keep from having to do this every update
+    }
+}
--- Assets/Scripts/GUI/HeroPanelStats.cs
+++ Assets/Scripts/GUI/HeroPanelStats.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HeroPanelStats : MonoBehaviour
-{
-    public Text heroName;
-    public Text heroHP;
-    public Text heroMP;
-    public Image progressBar;
-}
+//public class HeroPanelStats : MonoBehaviour
+//{
+//    public Text heroName;
+//    public Text heroHP;
+//    public Text heroMP;
+//    public Image progressBar;
+//}
